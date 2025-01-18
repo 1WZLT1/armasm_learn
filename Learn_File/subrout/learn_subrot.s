1e9d2ff5d35f156b6c39dfbcbb1e3e9e654d3c0b
1WZLT1 <1471147933@qq.com>
2025-01-18 22:03:59 +0800

    标签跳跃，还有特权模式交给调试器托管

---
+++ Learn_File/subrout/learn_subrot.s
@@ -0,0 +1,22 @@
+ AREA subrot, CODE, READONLY
+		ENTRY
+		
+		EXPORT __main
+__main 
+
+start 
+		MOV R0,#1
+		MOV R1,#2
+		BL  lable 
+		
+stop 
+		MOV R0,#0x18
+		LDR R1,=0x20026 ;MOV的赋值范围是 0 - 4095 较大的数需要使用LDR
+		SVC    #0xAB    ;这部分不一定正确得做rtt的时候注意一下 感觉应该是对的 因为在debug情况下确实不会向下执行了
+		
+
+lable 
+		ADD R0,R0,R1
+		BX  LR
+
+		END
